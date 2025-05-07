5ac8523398106f3c80e5825b7e4e65e905fca215
Bryn Mailer <bryndiupero@gmail.com>
2025-05-07 15:33:04 +1000

    feat: started parsing struct fields in derive builder project

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,39 @@
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput, Data, Fields};
+use quote::{quote, format_ident};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    let ident = &input.ident;
+    let builder_ident = format_ident!("{}Builder", ident);
+
+    let data = input.data;
+    match data {
+        Data::Struct(data) => {
+            let fields = data.fields;
+            match fields {
+                Fields::Named(named) => {
+
+                },
+                _ => todo!(),
+            }
+        },
+        _ => todo!(),
+    }
+
+    let tokens = quote! {
+        pub struct #builder_ident {
+
+        }
+
+        impl #ident {
+            pub fn builder() -> #builder_ident {
+                #builder_ident {}
+            }
+        }
+    };
+
+    TokenStream::from(tokens)
 }
